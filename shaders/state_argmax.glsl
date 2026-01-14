832912b69b5cf614fc8266d02bd5bbce7242ff61
Virteso <rasmus.lille0@gmail.com>
2026-01-14 22:09:53 +0200

    CS automata coordinates problem fixed

--- shaders/state_argmax.glsl
+++ shaders/state_argmax.glsl
@@ -2,7 +2,7 @@
 #version 450
 layout(local_size_x=8, local_size_y=8, local_size_z=8) in;
 
-layout(set=0, binding=0, std430) buffer State { uint data[]; } state;
+layout(set=0, binding=0, std430) buffer State { readonly uint data[]; } state;
 
 layout(set=0, binding=1, r8) uniform writeonly image3D out_types;
 
